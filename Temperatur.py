28fb818bf3a19d55b0e986cdaf5fe247d3b03d1b
BasharSaid <33035384+basharSaid@users.noreply.github.com>
2018-01-16 15:08:21 +0100

    Umrechnung von Celsius in Fahrenheit

---
+++ Temperatur.py
@@ -0,0 +1,10 @@
+print("Umrechnung von Celsius in Fahrenheit")
+
+# trennung einer Zeichenkette
+print("Bitte geben Sie eine Temperatur in Celsius ein:")
+TemperaturInCelsius = float(input())
+# Trennungg eines Ausdrucks
+TemperaturInFahrenheit = TemperaturInCelsius * 9/5 + 32
+# Trennung nach einem Komma
+print(TemperaturInCelsius, "Grad Celsius entsprechen", TemperaturInFahrenheit, "Grad Fahrenheit")
+      
