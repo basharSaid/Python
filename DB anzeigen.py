d6f59571ef461cbe30a570f41b54aeebabd19468
BasharSaid <33035384+basharSaid@users.noreply.github.com>
2018-01-23 12:42:50 +0100

    Datenbank erzeugen

---
+++ DB anzeigen.py
@@ -0,0 +1,20 @@
+import sqlite3
+# Verbindung, Cursor
+connection = sqlite3.connect("firma.db")
+cursor = connection.cursor()
+ # SQL- Abfrage
+sql = "SELECT* FROM personen"
+ 
+# Kontrollausgabe der SQL-Abfrage
+# print(sql)
+# Absenden der SQL-Abfrage
+# Empfang des Erbenisses
+cursor.execute(sql)
+
+# Ausgabedes Ergebnisses
+for dsatz in cursor:
+    print(dsatz[0], dsatz[1], dsatz[2], dsatz[3], dsatz[4])
+
+# Verbindung beenden
+connection.close()
+
