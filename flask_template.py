590e66ab1fcee2d21ea57ee8248cbb669aca0b70
BasharSaid <33035384+basharSaid@users.noreply.github.com>
2018-02-02 15:14:19 +0100

    Flask_HTML-Vorlagen verwenden

---
+++ flask_template.py
@@ -0,0 +1,19 @@
+
+
+# -*- coding:utf8 -*-
+from flask import Flask, render_template
+app = Flask(__name__)
+
+# Home Page
+@app.route("/")
+def home():
+    return render_template('test.html', page = u"الصّفحة الرّئيسيّة")
+
+# Hello Page
+@app.route("/hello/")
+def hello():
+    return render_template('test.html', page = u"صفحة التّرحيب")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
